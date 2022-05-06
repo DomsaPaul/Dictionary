bfeed007928b1bf61102d158e7e86db4b79424b3
Domsa Paul <104144984+DomsaPaul@users.noreply.github.com>
2022-05-06 12:55:47 +0300

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -1,37 +1,3 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Dictionary</title>
-  </head>
-  <body>
-    <div class="container">
-          <div class="card">
-        <div class="card-body">
-        <style> h1{text-align: center;}</style>
-        <h1>DICTIONARY</h1>
-    </div>
-  </div>
-</div>
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
-          <input type="text" placeholder="Enter the word" id="Add">
-          <button type="button" class="btn btn-success" onclick="return Add();">Add</button>
-      </div>
-    </div>
-  </div>
-    <div class="container">
-      <div class="card">
-        <div class="card-body">
-          <input type="text" placeholder="Enter the word" id="Search">
-          <button type="button" class="btn btn-primary" onclick="return Search();">Search</button>
-        </div>
-      </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script>
 var Dictionary = [" "];
   function Add(){
