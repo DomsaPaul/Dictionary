6b2a3b29fbc381b962f671f61464bf1cae9dd5ba
Domsa Paul <104144984+DomsaPaul@users.noreply.github.com>
2022-05-07 16:24:05 +0300

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -1,10 +1,10 @@
-let dictionary = [" "];
- function add(){
+  let dictionary = [" "];
+  function add(){
     let addWord = document.getElementById("add").value;
     dictionary.push(addWord);
     alert("The word \"" + addWord + "\" was added to the dictionary!");
-  }
- function search() {
+    }
+  function search() {
     let searchWord = document.getElementById("search").value;
     let exists = 0;
       for (let i = 0; i < dictionary.length; ++i){
@@ -12,10 +12,10 @@ let dictionary = [" "];
           exists = 1;        
         } 
       }
-        if (exists == 1) {
-          alert("The word \"" + searchWord + "\" already exists in the dictionary!");
-      } else {
-          alert("The word \"" + searchWord + "\" does not exist in the dictionary!");
-      }
-      return false;
+      if (exists == 1) {
+        alert("The word \"" + searchWord + "\" already exists in the dictionary!");
+    } else {
+        alert("The word \"" + searchWord + "\" does not exist in the dictionary!");
+    }
+    return false;
   }
