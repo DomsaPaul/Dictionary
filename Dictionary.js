2f8722ef0ae9f8640650dd32d2061b9faaa992cb
Domsa Paul <104144984+DomsaPaul@users.noreply.github.com>
2022-05-07 16:16:54 +0300

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -1,23 +1,21 @@
-<script>
-var Dictionary = [" "];
-  function Add(){
-    var AddWord = document.getElementById("Add").value;
-    Dictionary.push(AddWord);
-    alert("The word \"" + AddWord + "\" was added to the dictionary!");
-}
-  function Search() {
-    var SearchWord = document.getElementById("Search").value;
-    var exists = 0;
-      for (var i = 0; i < Dictionary.length; ++i){
-        if (SearchWord.match(Dictionary[i])){
+let dictionary = [" "];
+ function add(){
+    let addWord = document.getElementById("add").value;
+    dictionary.push(addWord);
+    alert("The word \"" + addWord + "\" was added to the dictionary!");
+  }
+ function search() {
+    let searchWord = document.getElementById("search").value;
+    let exists = 0;
+      for (let i = 0; i < dictionary.length; ++i){
+        if (searchWord.match(dictionary[i])){
           exists = 1;        
         } 
       }
         if (exists == 1) {
-          alert("The word \"" + SearchWord + "\" already exists in the dictionary!");
-      }else{
-          alert("The word \"" + SearchWord + "\" does not exist in the dictionary!");
+          alert("The word \"" + searchWord + "\" already exists in the dictionary!");
+      } else {
+          alert("The word \"" + searchWord + "\" does not exist in the dictionary!");
       }
       return false;
   }
-    </script>
